3a9efdb32f3335af0109ca1aef0af6b5ea01a004
k-ta-yamada <key.luvless@gmail.com>
2015-11-04 09:12:44 +0900

    Include and Extend support for the simple use #12

--- spec/tee_logger_spec.rb
+++ spec/tee_logger_spec.rb
@@ -7,6 +7,42 @@ describe TeeLogger do
   let(:block)     { proc { 'this is blocked message' } }
   let(:formatter) { proc { |s, _, _, m| "#{s}:#{m}\n" } }
 
+  describe 'extend_and_include' do
+    context 'extend Module' do
+      it 'can call #logger method' do
+        Mod = Module.new do
+          extend TeeLogger
+          def self.test
+            logger(fake_file).info 'hello'
+          end
+        end
+        console_result = tail_console { Mod.test }
+        logfile_result = tail_logfile
+
+        expected = regexp('info', Mod, 'hello')
+        expect(console_result).to all(match(expected))
+        expect(logfile_result).to all(match(expected))
+      end
+    end
+
+    context 'include Module' do
+      it 'can call #logger method' do
+        Klass = Class.new do
+          include TeeLogger
+          def test
+            logger(fake_file).info 'hello'
+          end
+        end
+        console_result = tail_console { Klass.new.test }
+        logfile_result = tail_logfile
+
+        expected = regexp('info', Klass.name, 'hello')
+        expect(console_result).to all(match(expected))
+        expect(logfile_result).to all(match(expected))
+      end
+    end
+  end
+
   describe 'logging_methods' do
     context 'only_message' do
       it_behaves_like 'logging', 'hello, world!', nil, nil
