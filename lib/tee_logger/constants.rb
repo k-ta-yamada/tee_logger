ac4015182359b802c6a855f504dc38af185cb01e
k-ta-yamada <key.luvless@gmail.com>
2015-11-07 14:42:07 +0900

    [clean] for Inch CI

--- lib/tee_logger/constants.rb
+++ lib/tee_logger/constants.rb
@@ -2,19 +2,27 @@
 module TeeLogger
   # no param of filename, set this filename
   DEFAULT_FILE = './tee_logger.log'
+
   # Implements targets
   LOGGING_METHODS = [:debug, :info, :warn, :error, :fatal]
+
   # defined logdev names
   LOGDEV_NAMES = [:console, :logfile]
+
   # defined paired of logdev name
   LOGDEV_REVERSE = { console: :logfile, logfile: :console }
 
+  # empty format
   FORMATTER_FOR_DISABLING = proc { |_severity, _time, _progname, _msg| '' }
 
-  # using private method #parse_to_hash
+  # using TeeLogger::Utils.extract_options
+  # @param logdev_name [Symbol]
+  # @param indent_level [Fixnum]
   ParsedOption = Struct.new(:logdev_name, :indent_level)
+
   # LOGDEV_NAMES not included error
   class IncorrectNameError < StandardError; end
+
   # option's class is not allow
   class IncorrectOptionError < StandardError; end
 end
