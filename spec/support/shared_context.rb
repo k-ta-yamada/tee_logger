3777ef180f5251a20acb37ebeedb6ec6f23ec642
k-ta-yamada <key.luvless@gmail.com>
2016-01-14 10:01:58 +0900

    [modify] complete specs. and solved that warning of redefine Constant.

--- spec/support/shared_context.rb
+++ spec/support/shared_context.rb
@@ -7,7 +7,12 @@ shared_context 'shared_context' do
   let(:datetime_format_reg_exp) { '\d{8}\s\d{6}' }
 
   let(:klass) do
-    Klass ||= Class.new do # rubocop:disable Style/ConstantName
+    # テストのとき、定数を再代入した時のwarningを止めたい - 福島餃子Ruby親方
+    # @ref http://xibbar.hatenablog.com/entry/20091126/1259216726
+    # How to redefine a Ruby constant without warning? - Stack Overflow
+    # @ref http://stackoverflow.com/questions/3375360/how-to-redefine-a-ruby-constant-without-warning
+    Object.send(:remove_const, 'Klass') if defined?(Klass)
+    Klass = Class.new do
       extend TeeLogger
       include TeeLogger
     end
@@ -15,4 +20,13 @@ shared_context 'shared_context' do
 
   before { TeeLogger.configure { |c| c.logdev = fake_file } }
   after  { TeeLogger.configuration_reset }
+
+  # for tee_logger_spec.rb
+  # TeeLogger configuration is reflected
+  let(:expected) do
+    { level:           regexp(:debug, klass, message),
+      progname:        regexp(:debug, progname, message),
+      datetime_format: regexp(:debug, klass, message, datetime_format_reg_exp),
+      formatter:       Regexp.new("#{:debug.upcase}:#{message}") }
+  end
 end
--- spec/tee_logger_spec.rb
+++ spec/tee_logger_spec.rb
@@ -4,78 +4,65 @@ describe TeeLogger do
   include_context 'shared_context'
 
   describe '.new' do
-    it '' do
-      expect(described_class.new(fake_file)).to be_a(TeeLogger::TeeLoggerBase)
-    end
+    subject { described_class.new }
+    it { is_expected.to be_a(TeeLogger::TeeLoggerBase) }
   end
 
-  shared_examples 'extend_or_include' do |type, msg, sev|
-    it "can call .#logger method : #{msg}" do
-      target = { extended: klass,
-                 included: klass.new }[type]
+  describe 'extend_or_include' do
+    shared_examples 'extend_or_include' do |type, msg, sev|
+      it "can call .#logger method : #{msg}" do
+        target = { extended: klass,
+                   included: klass.new }[type]
 
-      console_result = tail_console { target.logger.send(sev, msg) }
-      logfile_result = tail_logfile
+        console_result = tail_console { target.logger.send(sev, msg) }
+        logfile_result = tail_logfile
 
-      expected = regexp(sev, klass, msg)
-      expect(console_result).to all(match(expected))
-      expect(logfile_result).to all(match(expected))
-    end
-  end
-
-  it_behaves_like 'extend_or_include', :extended, 'case extended', :info
-  it_behaves_like 'extend_or_include', :included, 'case included', :debug
-
-  describe 'setting is reflected' do
-    before do
-      # TeeLogger.configuration_reset
-      TeeLogger.configure do |config|
-        config.logdev   = default_file
-        config.level    = Logger::Severity::INFO
-        config.progname = progname
+        expected = regexp(sev, klass, msg)
+        expect(console_result).to all(match(expected))
+        expect(logfile_result).to all(match(expected))
       end
+    end
+
+    it_behaves_like 'extend_or_include', :extended, 'case extended', :info
+    it_behaves_like 'extend_or_include', :included, 'case included', :debug
+  end
+
+  describe 'configuration is reflected' do
+    shared_examples 'configurations' do |type|
+      it "with #{type}" do
+        console_result = tail_console { klass.logger.debug message }
+        logfile_result = tail_logfile
+
+        # p console_result
+        # p logfile_result
+
+        expected_size = (type == :level) ? 0 : 1
+        expect(console_result.size).to eq(expected_size)
+        expect(logfile_result.size).to eq(expected_size)
+
+        expect(console_result).to all(match(expected[type]))
+        expect(logfile_result).to all(match(expected[type]))
+      end
+    end
+
+    context 'with level' do
+      before { TeeLogger.configure { |c| c.level = Logger::Severity::INFO } }
+      it_behaves_like 'configurations', :level
+    end
+
+    context 'with progname' do
+      before { TeeLogger.configure { |c| c.progname = progname } }
+      it_behaves_like 'configurations', :progname
     end
 
     context 'with datetime_format' do
-      before do
-        TeeLogger.configure { |c| c.datetime_format = datetime_format }
-      end
-
-      xit '' do
-        console_result = tail_console do
-          klass.logger.debug message
-          klass.logger.info message
-        end
-        logfile_result = tail_logfile(10, default_file)
-
-        expect(console_result.size).to eq(1)
-        expect(logfile_result.size).to eq(1)
-
-        expected = regexp(:info, progname, message, datetime_format_reg_exp)
-        expect(console_result).to all(match(expected))
-        expect(logfile_result).to all(match(expected))
-      end
+      before { TeeLogger.configure { |c| c.datetime_format = datetime_format } }
+      it_behaves_like 'configurations', :datetime_format
     end
 
     context 'with formatter' do
-      before do
-        TeeLogger.configure { |c| c.formatter = formatter }
-      end
-
-      xit '' do
-        console_result = tail_console do
-          klass.logger.debug message
-          klass.logger.info message
-        end
-        logfile_result = tail_logfile(10, default_file.path)
-
-        expect(console_result.size).to eq(1)
-        expect(logfile_result.size).to eq(1)
-
-        expected = Regexp.new("#{:info.upcase}:#{message}")
-        expect(console_result).to all(match(expected))
-        expect(logfile_result).to all(match(expected))
-      end
+      before { TeeLogger.configure { |c| c.formatter = formatter } }
+      it_behaves_like 'configurations', :formatter
     end
   end
 end
